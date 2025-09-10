65553eb8d66ad086ead84c2489ea77360a1d5623
0sinach1 <osimachifeanyi@gmail.com>
2025-09-10 23:06:38 +0100

    login persistence & shipping details

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm,ChangePasswordForm,UserInfoForm
 from django import forms
 from django.db.models import Q
+import json
+from cart.cart import Cart
 
 
 def search(request):
@@ -118,6 +120,18 @@ def login_user(request):
         user = authenticate(request, username=username, password= password)
         if user is not None:
             login(request, user)
+            
+            current_user = Profile.objects.get(user__id = request.user.id)
+            
+            saved_cart = current_user.old_cart
+            
+            if saved_cart:
+                converted_cart = json.loads(saved_cart)
+                
+                cart = Cart(request)
+                for key, value in converted_cart.items():
+                    cart.db_add(product = key, quantity = value)
+            
             messages.success(request, ('You have been logged in successfully'))
             return redirect('home')
         else:
@@ -141,7 +155,7 @@ def register_user(request):
             password = form.cleaned_data['password1']
     # log in users
             user = authenticate(username = username, password = password)
-            login(request, user)
+            login(request, user)           
             messages.success(request, ('You have registrered successfully, Welcome'))
             return redirect('update_user')
         else:
