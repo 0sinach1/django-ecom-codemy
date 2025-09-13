f22c6ee28865c348063941817bb48e5bdf9f7081
0sinach1 <osimachifeanyi@gmail.com>
2025-09-13 03:48:19 +0100

    Things

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -1,4 +1,51 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from payment.forms import ShippingForm, PaymentForm
+from payment.models import ShippingAddress
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+
+
 
 def payment_success(request):
     return render(request, 'payment/payment_success.html', {})
+
+
+def checkout(request):
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    
+    if request.user.is_authenticated:
+        shipping_user, created = ShippingAddress.objects.get_or_create(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, 'payment/checkout.html', {"cart_products" : cart_products, 'quantities' : quantities, 'totals' : totals, 'shipping_form':shipping_form})
+
+    else:
+        shipping_user, created = ShippingAddress.objects.get_or_create(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, 'payment/checkout.html', {"cart_products" : cart_products, 'quantities' : quantities, 'totals' : totals, 'shipping_form' : shipping_form})
+    
+def billing_info(request):
+    if request.POST:
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        
+        if request.user.is_authenticated:
+            billing_form = PaymentForm()
+            
+            return render(request, 'payment/billing_info.html', {"cart_products" : cart_products, 'quantities' : quantities, 'totals' : totals, 'shipping_info':request.POST, 'billing_form': billing_form})
+
+        else:
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html', {"cart_products" : cart_products, 'quantities' : quantities, 'totals' : totals, 'shipping_info':request.POST, 'billing_form': billing_form})
+
+        
+        shipping_info = request.POST
+        return render(request, 'payment/billing_info.html', {"cart_products" : cart_products, 'quantities' : quantities, 'totals' : totals, 'shipping_form':shipping_form})
+    else:
+        messages.success(request, 'Access Denied')
+        return redirect('home')
